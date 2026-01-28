5519209a605757f59d02a012ebb1a38e9fa21871
Krish KC <khatrikrish03@gmail.com>
2026-01-28 15:47:54 -0500

    added the logout feature 5

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -25,7 +25,7 @@ export class AuthInterceptor implements HttpInterceptor {
       return next.handle(req);
     }
 
-    if (token!) {
+    if (token) {
       const authReq = req.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`,
